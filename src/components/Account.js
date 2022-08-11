5c520a3af13f711807ed11c6ddb91064d046f66d
Michael Oberst <mioberst@gmx.de>
2022-08-11 14:40:39 +0200

    delete button to delete users

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,25 +1,11 @@
-import { useState, useEffect, useContext } from "react";
-import { NavLink, useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
 export function Account(props) {
     const { renderAreasOfInfluence } = props
-    const navigate = useNavigate()
     const { user, logOutUser } = useContext(AuthContext)
-    const { _id, email, username, type, organization, position, party, areasOfInfluence } = user
-    const authToken = localStorage.getItem("authToken")
-
-    const deleteAccount = (event) => {
-        axios.delete(`${process.env.REACT_APP_URL}/auth/user/${_id}`, { headers: { Authorization: `Bearer ${authToken}` } })
-            .then(() => {
-                logOutUser()
-                navigate("/")
-            })
-            .catch(err => {
-                console.log("An error has occurred while deleting a user:", err)
-            })
-    }
+    const { email, username, type, organization, position, party, areasOfInfluence } = user
 
     return (
         <div className="page">
@@ -37,9 +23,6 @@ export function Account(props) {
                 <NavLink to={`/edit/${type}`} className="link end-buttons">
                     <button>Edit Account</button>
                 </NavLink>
-                <form className="link end-buttons">
-                    <button type="button" onClick={deleteAccount}>Delete Account</button>
-                </form>
                 <NavLink to="/services" className="link end-buttons">
                     <button>Services</button>
                 </NavLink>
