b8f4228c0e2d1a6ac84f22718920c50b11cb2b58
Michael Oberst <mioberst@gmx.de>
2022-11-08 11:42:15 +0100

    install Bootstrap for React

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import {AuthProviderWrapper} from './context/auth.context';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
