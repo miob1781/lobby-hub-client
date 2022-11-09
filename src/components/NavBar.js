e5a3499234934363714158ee89ee797a54f3d487
Michael Oberst <mioberst@gmx.de>
2022-11-09 16:53:34 +0100

    style nav bar with bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,28 @@
-import {useState, useEffect, useContext} from "react";
-import {NavLink, useParams} from "react-router-dom";
-import {AuthContext} from "../context/auth.context";
+import { useContext } from "react";
+import { Nav, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { AuthContext } from "../context/auth.context";
 
-export function NavBar(props){
-    const {logOutUser, isLoggedIn} = useContext(AuthContext)
+export function NavBar() {
+    const { logOutUser, isLoggedIn } = useContext(AuthContext)
     return (
-        <nav>
-            <NavLink className="link" to="/">
-                <button>Home</button>
-            </NavLink>
-            <NavLink className="link" to="/services" style={{display: isLoggedIn ? "block" : "none"}}>
-                <button>Services</button>
-            </NavLink>
-            <NavLink className="link" to="/our-mission">
-                <button>Our Mission</button>
-            </NavLink>
-            {isLoggedIn ? 
-            <form className="link">
-                <button type="button" onClick={logOutUser}>Logout</button>
-            </form> :
-            <NavLink className="link" to="/login">
-                <button>Login</button>
-            </NavLink>}
-        </nav>
+        <Nav className="justify-content-center">
+            <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/services" style={{ display: isLoggedIn ? "block" : "none" }}>
+                <Nav.Link>Services</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/our-mission">
+                <Nav.Link>Our Mission</Nav.Link>
+            </LinkContainer>
+            {isLoggedIn
+                ? <form className="mb-3">
+                    <Button type="button" onClick={logOutUser}>Logout</Button>
+                </form>
+                : <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                </LinkContainer>}
+        </Nav>
     )
 }
